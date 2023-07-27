a3fbf4e8bed3a1b16b2adb1d15a19583de7556ad
Owen434 <128884918+Owen434@users.noreply.github.com>
2023-07-28 01:42:27 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'a0b61518-42ef-43e3-8bd6-fa6a836e5dd7';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
